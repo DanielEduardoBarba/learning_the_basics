a9ada95bda213a29ca7dfafa7e7363fa2839c44d
Daniel Barba <frdb103@gmail.com>
2024-07-10 07:57:02 -0400

    comments

---
+++ index.js
@@ -0,0 +1,35 @@
+//Welcome to learning the basics!
+//This file will become a template 
+//for you to experiment with and learn
+//the basics of Java Script
+
+//So lets begin :D
+
+//1st concept
+//-----------------Comments--------------
+//You have been looking at comments thus far.
+//In javascript comments are declared with ANYTHING
+//after the "//" symbol, in other languages it can be 
+//the "#" symbol or somethings "<-- -->"
+//You can also write it like..... 
+
+/*this! this is a way to wrap 
+multiple lines of code 
+into a large comment*/
+
+//simply put anything in between the "/*" and "*/" characters
+//Comments are NOT run as code, they are ignored by Java Script 
+//interpretters, so feel free to use them as you like!
+
+//They are used also to quickly show and ignore parts of code
+//Here is a little piece of code, declaring a simple variable
+//Put your cursor on the line of code, and hit
+// Shift + "/" for Ubuntu 
+// CMD + "/" for Macbook
+
+// const myVariable="I am a string!"
+
+
+//2nd concept
+//-------------Data Types-----------
+//Data types are
